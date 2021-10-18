7441eb18bd66ae65a3da6011eea7691ec3b91efb
Вахитов Рустам <therealrustam@yandex.ru>
2021-10-18 22:56:11 +0500

    Work very well

--- homework.py
+++ homework.py
@@ -1,3 +1,5 @@
+"""Телеграм бот-ассистент. Итоговый проект."""
+
 import logging
 import os
 import time
@@ -7,14 +9,14 @@ import telegram
 from dotenv import load_dotenv
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-
-RETRY_TIME = 300
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
 HOMEWORK_STATUSES = {
@@ -25,45 +27,55 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
+    """Метод отправки сообщении ботом."""
     bot.send_message(chat_id=CHAT_ID, text=message)
+    logging.info('Сообщение отправлено')
 
 
 def get_api_answer(url, current_timestamp):
+    """Метод отправки запроса к API."""
     payload = {'from_date': current_timestamp}
     response = requests.get(
         ENDPOINT, headers=headers, params=payload)
     if response.status_code != 200:
         logging.error(
-            f'<The answer is not 200>: {response.status_code}')
-        raise requests.HTTPError('<The answer is not 200>')
+            f'Код состояния HTTP не равен 200: {response.status_code}')
+        raise requests.HTTPError('Код состояния HTTP не равен 200')
     response = response.json()
     return response
 
 
 def parse_status(homework):
-    verdict = homework[0]['status']
+    """Метод анализа статуса и подготовки сообщения."""
+    verdict = homework[0].get('status')
     verdict = HOMEWORK_STATUSES[verdict]
-    homework_name = homework.get('homework_name')
+    homework_name = homework[0].get('homework_name')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_response(response):
-    homeworks = response.get('homeworks')
-    parse_status(homeworks)
+    """Метод проверки полученного ответа на корректность и проверка статуса."""
+    print(response)
+    homework = response['homeworks']
+    return homework
 
 
 def main():
+    """Метод выполнения основного кода."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
     current_timestamp = current_timestamp - 2592000
     while True:
         try:
             response = get_api_answer(ENDPOINT, current_timestamp)
-            check_response(response)
+            homework = check_response(response)
+            message = parse_status(homework)
             time.sleep(RETRY_TIME)
             send_message(bot, message)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
+            logging.error(
+                f'Сбой в работе программы: {error}')
             send_message(bot, message)
             time.sleep(RETRY_TIME)
             continue
