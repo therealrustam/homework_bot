f07db4063b3184e2ab2df0730b833c62496d88aa
Вахитов Рустам <therealrustam@yandex.ru>
2021-10-19 22:25:54 +0500

    Almost done

--- homework.py
+++ homework.py
@@ -15,7 +15,8 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
+counter = {0: 0}
+old_status = {0: ''}
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 
@@ -47,37 +48,59 @@ def get_api_answer(url, current_timestamp):
 
 def parse_status(homework):
     """Метод анализа статуса и подготовки сообщения."""
-    verdict = homework[0].get('status')
+    verdict = homework.get('status')
     verdict = HOMEWORK_STATUSES[verdict]
-    homework_name = homework[0].get('homework_name')
+    homework_name = homework.get('homework_name')
+    logging.info('Изменился статус проверки работы')
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_response(response):
     """Метод проверки полученного ответа на корректность и проверка статуса."""
+    if response['homeworks'] == []:
+        logging.error('Нет данных')
+        raise Exception('Нет данных')
+    if response['homeworks'][0].get('status') not in ['approved',
+                                                      'reviewing',
+                                                      'rejected', ]:
+        logging.error('Неизвестный статус')
+        raise Exception('Неизвестный статус')
     print(response)
-    homework = response['homeworks']
-    return homework
+    if counter[0] == 0:
+        old_status[0] = response['homeworks'][0].get('status')
+        counter[0] = 1
+        return old_status[0]
+    if counter[0] == 1:
+        status = response['homeworks'][0].get('status')
+        if status == old_status[0]:
+            return False
+        else:
+            return status
 
 
 def main():
     """Метод выполнения основного кода."""
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    current_timestamp = current_timestamp - 2592000
+    current_timestamp = current_timestamp - RETRY_TIME
     while True:
         try:
             response = get_api_answer(ENDPOINT, current_timestamp)
-            homework = check_response(response)
-            message = parse_status(homework)
+            if check_response(response):
+                homework = response['homeworks'][0]
+                message = parse_status(homework)
+                send_message(bot, message)
             time.sleep(RETRY_TIME)
-            send_message(bot, message)
+            current_timestamp = int(time.time())
+            current_timestamp = current_timestamp - RETRY_TIME
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logging.error(
                 f'Сбой в работе программы: {error}')
             send_message(bot, message)
             time.sleep(RETRY_TIME)
+            current_timestamp = int(time.time())
+            current_timestamp = current_timestamp - RETRY_TIME
             continue
 
 
